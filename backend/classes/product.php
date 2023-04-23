ae01d7f4814e396633d6813a0304f0a542b5790a
swagoverlord <azcooldude8@gmail.com>
2023-04-24 00:57:44 +0200

    Base product class

---
+++ backend/classes/product.php
@@ -0,0 +1,57 @@
+<?php
+
+abstract class Product
+{
+    private $sku;
+    private $name;
+    private $price;
+    private $type;
+
+    public function __construct($sku, $name, $price, $type)
+    {
+        $this->sku = $sku;
+        $this->name = $name;
+        $this->price = $price;
+        $this->type = $type;
+    }
+
+    public function getSku()
+    {
+        return $this->sku;
+    }
+
+    public function setSku($sku)
+    {
+        $this->sku = $sku;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
