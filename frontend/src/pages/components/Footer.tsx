d916e3eb604b84b7ed3ce630b66532db31a944d5
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-23 17:48:01 +0200

    Base footer

---
+++ frontend/src/pages/components/Footer.tsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Scandiweb Test Assignment</h1>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
