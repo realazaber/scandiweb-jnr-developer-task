137d4c8d12b0f2ec47942bbb90cbb7b34a61add1
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-23 17:48:25 +0200

    Page and page container base layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/styles.scss";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12}>
+          <Nav />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Component {...pageProps} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Footer />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
