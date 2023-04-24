4cd595c8fbeda157fcfbfe58153a7aab903f756a
swagoverlord <azcooldude8@gmail.com>
2023-04-24 11:55:25 +0200

    Testing endpoint

--- backend/index.php
+++ backend/index.php
@@ -1,2 +1,9 @@
 <?php
-    echo "Hello world";
+
+$baseUrl = "/projects/scandiweb-jnr-developer-task/backend";
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
+    header('Content-Type: application/json');
+    $response = array('message' => 'Hello, world!');
+    echo json_encode($response);
+}
