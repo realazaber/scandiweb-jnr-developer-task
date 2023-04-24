55d4dbda530511fb0c593a5c2503bee6b42a2afe
swagoverlord <azcooldude8@gmail.com>
2023-04-24 15:37:03 +0200

    base endpoints

--- backend/index.php
+++ backend/index.php
@@ -2,8 +2,23 @@
 
 $baseUrl = "/projects/scandiweb-jnr-developer-task/backend";
 
+//Get products URL
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
     header('Content-Type: application/json');
-    $response = array('message' => 'Hello, world!');
+    $response = array('message' => 'Get products');
+    echo json_encode($response);
+}
+
+//Add product URL
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
+    header('Content-Type: application/json');
+    $response = array('message' => 'Add product');
+    echo json_encode($response);
+}
+
+//Delete products URL
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
+    header('Content-Type: application/json');
+    $response = array('message' => 'Delete product');
     echo json_encode($response);
 }
