e44883f7673648be5d41697e6aea2efa44b23f1c
swagoverlord <azcooldude8@gmail.com>
2023-04-24 17:10:19 +0200

    Product interface

---
+++ frontend/src/interfaces/Product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: Number;
+  sku: String;
+  price: Number;
+  type: String;
+
+  weight?: Number;
+
+  megabytes?: Number;
+
+  width?: Number;
+  height?: Number;
+  depth?: Number;
+}
