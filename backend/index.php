b6c5cb1d4c4f98a568d4694df0a795c389d1030c
swagoverlord <azcooldude8@gmail.com>
2023-04-24 16:51:35 +0200

    Get products works

--- backend/index.php
+++ backend/index.php
@@ -1,29 +1,93 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once("config.php");
+require("./classes/book.php");
+require("./classes/dvd.php");
+require("./classes/furniture.php");
 
-$connection = new Config();
-$connection->connect();
+class ProductAPI
+{
+    private $pdo;
+    private $baseUrl;
 
-$baseUrl = "/projects/scandiweb-jnr-developer-task/backend";
+    public function __construct()
+    {
+        $this->pdo = new Config();
+        $this->baseUrl = "/projects/scandiweb-jnr-developer-task/backend";
+    }
 
-//Get products URL
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
-    header('Content-Type: application/json');
-    $response = array('message' => 'Get products');
-    echo json_encode($response);
+    public function handleRequest()
+    {
+        $request_method = $_SERVER['REQUEST_METHOD'];
+        $request_uri = $_SERVER['REQUEST_URI'];
+
+        switch ("$request_method $request_uri") {
+            case "GET {$this->baseUrl}/products":
+                $this->getProducts();
+                break;
+            case "POST {$this->baseUrl}/products":
+                $this->addProduct();
+                break;
+            case "DELETE {$this->baseUrl}/products":
+                $this->deleteProduct();
+                break;
+            default:
+                http_response_code(404);
+                echo json_encode(array('success' => false, 'message' => 'Invalid API endpoint'));
+        }
+    }
+
+    private function getProducts()
+    {
+        header('Content-Type: application/json');
+
+
+        try {
+            $stmt = $this->pdo->query("SELECT * FROM products");
+            $products = array();
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $product = null;
+                switch ($row['type']) {
+                    case 'DVD':
+                        $product = new DVD($row['sku'], $row['name'], $row['price'], $row['megabytes']);
+                        break;
+                    case 'book':
+                        $product = new Book($row['sku'], $row['name'], $row['price'], $row['weight']);
+                        break;
+                    case 'furniture':
+                        $product = new Furniture($row['sku'], $row['name'], $row['price'], $row['width'], $row['depth'], $row['height']);
+                        break;
+                }
+
+                if ($product) {
+                    $products[] = $product->toArray();
+                }
+            }
+            http_response_code(200);
+            echo json_encode(array('success' => true, 'data' => $products));
+        } catch (PDOException $e) {
+            http_response_code(500);
+            echo json_encode(array('success' => false, 'message' => 'Error retrieving products: ' . $e->getMessage()));
+        }
+    }
+
+    private function addProduct()
+    {
+        // TODO: implement addProduct method
+        http_response_code(501);
+        echo json_encode(array('success' => false, 'message' => 'Not implemented'));
+    }
+
+    private function deleteProduct()
+    {
+        // TODO: implement deleteProduct method
+        http_response_code(501);
+        echo json_encode(array('success' => false, 'message' => 'Not implemented'));
+    }
 }
 
-//Add product URL
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
-    header('Content-Type: application/json');
-    $response = array('message' => 'Add product');
-    echo json_encode($response);
-}
-
-//Delete products URL
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE' && $_SERVER['REQUEST_URI'] === $baseUrl . '/products') {
-    header('Content-Type: application/json');
-    $response = array('message' => 'Delete product');
-    echo json_encode($response);
-}
+$productAPI = new ProductAPI();
+$productAPI->handleRequest();
