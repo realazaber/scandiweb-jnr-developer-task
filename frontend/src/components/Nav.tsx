d57c52cb0303ab6dff365a6b304c94c33e89ecce
swagoverlord <azcooldude8@gmail.com>
2023-04-26 17:53:22 +0200

    Fixed a bug where you could delete products on add page

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -8,28 +8,34 @@ import { baseUrl } from "@/helper";
 
 export default function CustomNav() {
   const deleteProducts = () => {
-    let productsStr = localStorage.getItem("selectedProducts");
-    let productsArr = JSON.parse(productsStr);
+    try {
+      let productsStr = localStorage.getItem("selectedProducts");
+      let productsArr = JSON.parse(productsStr);
 
-    if (productsArr.length <= 0) {
-      alert("Please select at least one product");
-    } else {
-      productsArr.forEach((prodSku) => {
-        console.log("prod: ", prodSku);
-        fetch(baseUrl + "/products", {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ sku: prodSku }),
-        })
-          .then((res) => res.json())
-          .then((data) => console.log(data))
-          .catch((err) => console.error(err));
-      });
-      localStorage.removeItem("selectedProducts");
-      window.location.reload();
-    }
+      if (productsArr.length <= 0) {
+        alert("Please select at least one product");
+      } else {
+        productsArr.forEach((prodSku) => {
+          console.log("prod: ", prodSku);
+          fetch(baseUrl + "/products", {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ sku: prodSku }),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+        });
+        localStorage.removeItem("selectedProducts");
+        window.location.reload();
+      }
+    } catch (error) {}
+  };
+
+  const clearSelected = () => {
+    localStorage.removeItem("selectedProducts");
   };
 
   return (
@@ -42,7 +48,9 @@ export default function CustomNav() {
             </Col>
             <Col xs={7} className="d-flex justify-content-end">
               <Link href="/product_add">
-                <Button variant="success">Add</Button>
+                <Button onClick={clearSelected} variant="success">
+                  Add
+                </Button>
               </Link>
               <Link href="/">
                 <Button onClick={deleteProducts} variant="danger">
