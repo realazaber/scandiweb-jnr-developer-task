5b99cab7fc55ec8d2c822de9fba3e036fdc8f61d
swagoverlord <azcooldude8@gmail.com>
2023-04-27 17:25:26 +0200

    Update helper.ts

--- frontend/src/helper.ts
+++ frontend/src/helper.ts
@@ -1,2 +1,4 @@
-export const baseUrl: String =
+export const oldBaseUrl: String =
   "http://localhost/projects/scandiweb-jnr-developer-task/backend";
+
+export const baseUrl: String = "https://scandiweb-jnr-test.azaber.com";
